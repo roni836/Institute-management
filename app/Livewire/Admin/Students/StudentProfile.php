cfdd9b22858fe96345e65556e14e148d0b43fdb3
shaique <Shaique.9434@gmail.com>
2025-08-22 10:30:25 +0530

    dynamic student profile view with batch and payment details

--- app/Livewire/Admin/Students/StudentProfile.php
+++ app/Livewire/Admin/Students/StudentProfile.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Admin\Students;
 
+use App\Models\Batch;
 use App\Models\Student;
+use App\Models\Transaction;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\On;
 use Livewire\Component;
@@ -12,13 +14,15 @@ use Livewire\Component;
 class StudentProfile extends Component
 {
     public $student;
-
+    public $batch;
+    public $payments;
     public function mount($id)
     {
         $this->student = Student::with([
-           
             'admissions'
         ])->findOrFail($id);
+        $this->batch = Batch::whereIn('id', $this->student->admissions->pluck('batch_id'))->first();
+        $this->payments = Transaction::where('admission_id', $this->student->admissions->first()->id)->get();
     }
     public function render()
     {
